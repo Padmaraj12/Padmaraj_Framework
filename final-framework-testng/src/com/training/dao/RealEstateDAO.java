8948706db2adf533ba4201932f350abbf667c169
Padmaraj12 <46881344+Padmaraj12@users.noreply.github.com>
2019-02-16 21:51:00 +0530

    Add files via upload

---
+++ final-framework-testng/src/com/training/dao/RealEstateDAO.java
@@ -0,0 +1,61 @@
+package com.training.dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.training.bean.RegisterBean;
+import com.training.connection.GetConnection;
+import com.training.utility.LoadDBDetails;
+
+public class RealEstateDAO {
+
+	Properties properties;
+
+	public RealEstateDAO() {
+		try {
+			properties = new Properties();
+			FileInputStream inStream = new FileInputStream("./resources/sql.properties");
+			properties.load(inStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public List<RegisterBean> getRegions() {
+		String sql = properties.getProperty("get.regions");
+
+		GetConnection gc = new GetConnection();
+		List<RegisterBean> list = null;
+		try {
+			gc.ps1 = GetConnection.getMySqlConnection(LoadDBDetails.getDBDetails()).prepareStatement(sql);
+			list = new ArrayList<RegisterBean>();
+
+			gc.rs1 = gc.ps1.executeQuery();
+
+			while (gc.rs1.next()) {
+
+				RegisterBean temp = new RegisterBean();
+				temp.seteMail(gc.rs1.getString(1));
+				temp.setFirstName(gc.rs1.getString(2));
+				temp.setLastName(gc.rs1.getString(3));
+
+				list.add(temp);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	public static void main(String[] args) {
+		new RealEstateDAO().getRegions().forEach(System.out::println);
+	}
+
+}
