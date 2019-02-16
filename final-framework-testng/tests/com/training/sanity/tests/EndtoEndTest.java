f95ee9587118a239df944223d40bbbf5e24649e4
Padmaraj12 <46881344+Padmaraj12@users.noreply.github.com>
2019-02-16 22:18:26 +0530

    Add files via upload

--- final-framework-testng/tests/com/training/sanity/tests/EndtoEndTest.java
+++ final-framework-testng/tests/com/training/sanity/tests/EndtoEndTest.java
@@ -5,30 +5,28 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.training.generics.ScreenShot;
-import com.training.pom.AllProperties;
+import com.training.pom.EndtoEnd;
 import com.training.pom.LoginPOM;
-import com.training.pom.Propertiestab;
-import com.training.pom.Userslink;
 import com.training.utility.DriverFactory;
 import com.training.utility.DriverNames;
 
-public class UserslinkTest {
+public class EndtoEndTest {
 
 	private WebDriver driver;
 	private String baseUrl;
 	private LoginPOM loginPOM;
 	private static Properties properties;
 	private ScreenShot screenShot;
-	private Propertiestab propertiestab;
-	private AllProperties allProperties;
-	private Userslink userslink;
+	private EndtoEnd endtoEnd;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws IOException {
@@ -41,9 +39,8 @@ public class UserslinkTest {
 	public void setUp() throws Exception {
 		driver = DriverFactory.getDriver(DriverNames.CHROME);
 		loginPOM = new LoginPOM(driver);
+		endtoEnd = new EndtoEnd(driver);
 		baseUrl = properties.getProperty("baseURL");
-		userslink = new Userslink(driver);
-		allProperties = new AllProperties(driver);
 		screenShot = new ScreenShot(driver);
 		driver.manage().timeouts().implicitlyWait(Integer.parseInt(properties.getProperty("implicitWait")),
 				TimeUnit.SECONDS);
@@ -54,7 +51,7 @@ public class UserslinkTest {
 	@AfterMethod
 	public void tearDown() throws Exception {
 		Thread.sleep(1000);
-		// driver.quit();
+		//driver.quit();
 	}
 
 	@Test
@@ -63,23 +60,25 @@ public class UserslinkTest {
 		loginPOM.sendUserName("admin");
 		loginPOM.sendPassword("admin@123");
 		loginPOM.clickLoginBtn();
+		screenShot.captureScreenShot("First");
 
-		userslink.clickUsers();
-		Thread.sleep(3000);
-		userslink.clickAddNew();
-		userslink.sendUserName("manzoor");
-		userslink.sendEmail("manzoor@gmail.com");
-		userslink.sendFirstname("manzoor");
-		userslink.sendLastname("mehadi");
-		userslink.sendUrl("www.google.com");
-		userslink.clickPassword();
-		userslink.clickUserNotification();
-		userslink.selectRole();
-		userslink.clickAddNewUser();
-		Thread.sleep(3000);
-		System.out.println(userslink.getnewuser());
+		endtoEnd.clickPosts();
 		Thread.sleep(5000);
-		userslink.clickAllUsers();
-
+		endtoEnd.clickCategories();
+		endtoEnd.sendTagName("New Launches");
+		endtoEnd.sendSlug("launch");
+		endtoEnd.sendDescription("New Launches of villas, apartments, flats");
+		endtoEnd.clickAddNew();
+		endtoEnd.clickAllposts();
+		endtoEnd.clickAddNew();
+		endtoEnd.sendTitle("New Launches");
+		endtoEnd.sendTextBox(" New Launch in Home");
+		endtoEnd.clickAllCategories();
+		endtoEnd.clickPublish();
+		Actions action= new Actions(driver);
+		action.moveToElement(driver.findElement(By.xpath("//img[@class='avatar avatar-26 photo']"))).build().perform();
+		endtoEnd.clickLogOut();
+	
 	}
+	
 }
